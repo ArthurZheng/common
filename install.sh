347f9f9e84da9a15dd973732c71f8d057a88338b
Damien Whitten <damien@rebase.com.au>
2015-10-03 11:19:50 +1000

    Fixed install script for missing symlinks

--- install.sh
+++ install.sh
@@ -5,13 +5,11 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $HOME
 for file in `ls $DIR/dotfiles`; do
 	echo ".$file"
-	if [ -e ".$file" ]; then
-		if [ -h ".$file" ]; then
-			rm ".$file"
-		else
-			echo ".$file exists and is not a symlink" 
-			exit 1
-		fi
+	if [ -h ".$file" ]; then
+		rm ".$file"
+	elif [ -e ".$file" ]; then
+		echo ".$file exists and is not a symlink" 
+		exit 1
 	fi
 	ln -s "$DIR/dotfiles/$file" ".$file"
 done
