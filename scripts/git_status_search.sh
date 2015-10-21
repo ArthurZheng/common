6f992edde5620a6aed6d76c41f7850d12cb16233
Damien Whitten <damien@rebase.com.au>
2015-10-22 08:01:23 +1100

    Better Searching

--- scripts/git_status_search.sh
+++ scripts/git_status_search.sh
@@ -4,12 +4,15 @@ set -ue
 
 check() {
 	r=$1
+	if [ -d $r/.git ]; then
 	cd $r
 	echo "=========================="
 	pwd
+	remote=$(git config --get remote.origin.url)
+	echo $remote
 
     git_status=$(git status 2> /dev/null)
-	echo git_status
+	echo $git_status
 
 	if [[ ! $git_status =~ "working directory clean" ]]; then
 			echo "FIX $r"
@@ -24,28 +27,36 @@ check() {
 	elif [[ $git_status =~ "nothing to commit" ]]; then
 			echo "OK $r"
 	fi
+	fi
 	
 }
 
 folderChildren() {
 	parent=$1
+	if [ -d $parent ]; then
 	for folder in `ls $parent`; do
-		check $parent/$folder
+		if [ -d $parent/$folder ]; then
+			check $parent/$folder
+		fi
 	done
+	fi
 }
 
 search() {
 	parent=$1
+	if [ -d $parent ]; then
 	for folder in `find $parent -name ".git"`; do
-		check $folder/../
+		check $(realpath $folder/../)
 	done
+	fi
 }
 
 folderChildren ~/go/src/github.com/daemonl
 folderChildren ~/go/src/github.com/rmrf8
 folderChildren ~/go/src/github.com/TabDigital
+folderChildren ~/gbx
+folderChildren ~/schkit
+search ~/TabDigital
 search ~/ops
 check ~/common
 check ~/calite
-
-
